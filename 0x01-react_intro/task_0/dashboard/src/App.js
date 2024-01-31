a52927b777eff5d2bab0cee30f5556845b8b0a07
onyxvail <ons.hfi@gmail.com>
2024-01-31 14:19:51 +0100

    update task0/1

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,6 +1,5 @@
-import logo from "./Holberton Logo.jpg";
-import "./App.css";
-import { getFullYear, getFooterCopy } from "./utils";
+import logo from './holberton-logo.jpg';
+import './App.css';
 
 function App() {
   return (
@@ -9,19 +8,12 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
       </div>
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
-        <form>
-          <label htmlFor="email">Email:</label>
-          <input type="email" name="email"></input>
-          <label htmlFor="password">Password:</label>
-          <input type="password" name="password"></input>
-          <button>OK</button>
-        </form>
-      </div>
-      <div className="App-footer">
-        Copyright {getFullYear()} - {getFooterCopy()}
-      </div>
+      <div className='App-body'>
+				<p>Login to access the full dashboard</p>
+			</div>
+			<div className='App-footer'>
+				<p>Copyright 2020 - holberton School</p>
+			</div>
     </div>
   );
 }
